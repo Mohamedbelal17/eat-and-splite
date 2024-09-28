40c2d906e9990fae0a2658d359ea3f8cd88100e6
MohamedBelal17 <Mohamed.adBelal.69@gmail.com>
2024-09-28 22:15:28 +0300

    update branch only

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 
 //test the second update using github
 //Third test for update and commit using github
+//test the pull and edit on the branch only
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
